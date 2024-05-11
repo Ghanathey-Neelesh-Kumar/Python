3d4145e56c21f41c5f673a234a6718d61d17aec6
Ghanathey Neelesh Kumar <ghanatheyneelesh@gmail.com>
2024-05-11 18:20:26 +0100

    New File Updated

---
+++ Expression_Execution.py
@@ -0,0 +1,3 @@
+a, b =5, 10
+spl = "&"
+print(a*b*spl)
--- First_Program.py
+++ First_Program.py
@@ -4,3 +4,6 @@ price = 25.99
 
 print ("My name is :", name)
 print ("My age is : ", age)
+print(type(name))
+print(type(age))
+print(type(price))
