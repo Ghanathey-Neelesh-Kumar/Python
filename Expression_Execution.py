accfa2dbc2da2d71576acde3021d132c6c7421a6
Ghanathey Neelesh Kumar <ghanatheyneelesh@gmail.com>
2024-05-12 22:39:20 +0100

    Updated Code

--- Expression_Execution.py
+++ Expression_Execution.py
@@ -1,3 +1,19 @@
+  # String and Numeric Values can operate together with * 
 a, b =5, 10
 spl = "&"
 print(a*b*spl)
+
+# String and String can operate with +
+x, y = "5", 2
+txt = "@"
+print((x + txt)*y)
+
+# Result of division operator with two integers will be float
+e, f  = 20, 10
+c = e/f
+print(c)
+
+# Integer Division with float and int  will give integer but displayed as float
+
+m, n = 1.5, 3
+print(m//n)
