201fc94d195837f6fe2f8f680fa09dfa437fd836
Neelesh <ghanatheyneelesh@gmail.com>
2024-05-13 13:42:29 +0100

    New File

---
+++ Inputs_in_python.py
@@ -0,0 +1,11 @@
+# input() statement is used to accept the values from the user
+
+name = input("Enter you name: ")
+
+age = int(input("age : "))
+
+price = float(input("Price is : "))
+
+print(name)
+print(age)
+print(price)
