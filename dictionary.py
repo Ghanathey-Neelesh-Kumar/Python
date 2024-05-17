c1f51722e82787221c01b3b5e1e297b8055d27df
SwaChay <swapna.karna@gmail.com>
2024-05-17 10:00:53 +0100

    New feature added

---
+++ dictionary.py
@@ -0,0 +1,7 @@
+dict = {
+             "name" : "Neelesh",
+             "cgpa" : 9.6,
+             "marks": [98,95,99]
+        
+        }
+print(dict)
---
+++ functions.py
@@ -0,0 +1,17 @@
+# def cal_sum(a, b):
+#     s = a+b
+#     print(s)
+#     return s
+
+# cal_sum(5,10)
+# cal_sum(5,10)
+# cal_sum(5,10)
+# cal_sum(5,10)
+
+def avg_num(a,b,c):
+    avg =  (a+b+c)/3
+    print(avg)
+    return avg
+    
+ans = avg_num(5,5,5)
+print(ans)
